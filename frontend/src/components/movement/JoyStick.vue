6b957e0187043e612222ff71860759acff465ce6
Juan Guevara <soyjuandavidguevara@gmail.com>
2024-02-12 13:53:55 -0500

    moved images

--- frontend/src/components/movement/JoyStick.vue
+++ frontend/src/components/movement/JoyStick.vue
@@ -1,6 +1,4 @@
 <script setup>
-  import JoyStickBase from "@/assets/joystick-base.png";
-  import JoyStickHead from "@/assets/joystick-red.png";
   import { useStick } from "@/composables/useStick.js";
   import { useMovementService } from "@/services/movementService.js";
   import useInterval from "@/composables/useInterval.js";
@@ -23,9 +21,9 @@
 
 <template>
   <div class="w-[80px] h-[80px] relative">
-    <img :src="JoyStickBase" alt="JoyStick Base" class="w-100 cover" />
+    <img src="/img/joystick-base.png" alt="JoyStick Base" class="w-100 cover" />
     <div class="w-[30px] h-[30px] absolute top-0 bottom-0 left-0 right-0 m-auto" ref="stick">
-      <img :src="JoyStickHead" alt="JoyStick Head" class="w-100 cover" />
+      <img src="/img/joystick-red.png" alt="JoyStick Head" class="w-100 cover" />
     </div>
   </div>
 </template>
